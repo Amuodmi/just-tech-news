253b235c7d4f96c98c70f027bff2cbb15465513a
Michelle <michelle.douma@gmail.com>
2022-08-01 20:21:39 -0400

    doubting my life choices

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,17 @@
 
 const Sequelize = require('sequelize');
 
+
+require('dotenv').config();
+
 //create connection to our database, pass in your mySQL information for username and password
 
 //the new Sequelize function accepts the database name, mySQL username, and password, and passes configuration settings. 
-const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PASSWORD', {
-    host: 'localhost', 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
 
 //exporting the above connection 
 module.exports = sequelize; 
