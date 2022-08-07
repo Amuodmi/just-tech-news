c6cf9dbc354f947b14eacb7b4ab3ef063a34dea4
Michelle <michelle.douma@gmail.com>
2022-08-07 16:48:47 -0400

    updating repo

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -18,6 +32,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+//adds the session object to the request object, which will allow req.session to be defined
+app.use(session(sess));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
