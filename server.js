c9475d057c66c053d239902cb35a2fa26bddfd16
Michelle <michelle.douma@gmail.com>
2022-08-08 16:04:33 -0400

    added helpers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -21,7 +22,7 @@ const sess = {
 };
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
